263442bdfb8b329f55d2edc82632777b55026f97
CoreyGaunt <65140255+CoreyGaunt@users.noreply.github.com>
2022-11-30 21:25:56 -0600

    Refactor: creating a customer specific subscriptions prep model

---
+++ models/prep/prep_customer_subscriptions.sql
@@ -0,0 +1,29 @@
+select
+    sha256(concat(
+        cast(customer_id as string)
+        , cast(created_at as string)
+    )) as primary_key
+    , customer_id
+    , subscription_sku
+    , subscription_title
+    , subscription_price
+    , is_subscription_cancelled
+    , is_subscription_active
+    , is_subscription_prepaid
+    , is_skippable
+    , is_swappable
+    , sku_override
+    , max_retries_reached
+    , quantity
+    , created_at
+    , updated_at
+    , cancelled_at
+    , cancellation_reason
+    , cancellation_reason_comments
+    , variant_title
+    , shopify_product_id
+    , shopify_variant_id
+    , recharge_product_id
+    , next_charge_scheduled_at
+    , expire_after_specific_number_of_charges
+from {{ ref('staging_acme_recharge_subscriptions')}}
