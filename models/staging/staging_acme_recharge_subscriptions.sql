5c645446a43d754de980116f40ba129e9b5b3b6e
CoreyGaunt <65140255+CoreyGaunt@users.noreply.github.com>
2022-11-30 21:25:10 -0600

    Refactor: create staging table for dbt project

---
+++ models/staging/staging_acme_recharge_subscriptions.sql
@@ -0,0 +1,59 @@
+select 
+    sha256(recharge_subscriptions.id) as primary_key
+    , cast(recharge_subscriptions.id) as transaction_id
+    , cast(recharge_subscriptions.customer_id as int) as customer_id
+    , cast(recharge_subscriptions.sku as string) as subscription_sku
+    , cast(recharge_subscriptions.email as string) as customer_email
+    , cast(recharge_subscriptions.product_title as string) as subscription_title
+    , cast(recharge_subscriptions.price as float) as subscription_price
+    , case 
+        when recharge_subscriptions.status = 'CANCELLED' then TRUE
+        else FALSE 
+    end as is_subscription_cancelled
+    , case 
+        when recharge_subscriptions.status = 'ACTIVE' then TRUE 
+        else FALSE 
+    end as is_subscription_active
+    , cast(recharge_subscriptions.quanity as int) as quantity
+    , cast(recharge_subscriptions.created_at as datetime) as created_at
+    , cast(recharge_subscriptions.is_prepaid as bool) as is_subscription_prepaid
+    , recharge_subscriptions.properties
+    , cast(recharge_subscriptions.updated_at as datetime) as updated_at
+    , cast(recharge_subscriptions.cancelled_at as datetime) as cancelled_at
+    , cast(recharge_subscriptions.is_skippable as bool) as is_skippable
+    , cast(recharge_subscriptions.is_swappable as bool) as is_swappable
+    , cast(recharge_subscriptions.sku_override as bool) as sku_override 
+    , recharge_subscriptions.commit_update
+    , recharge_subscriptions.variant_title
+    , recharge_subscriptions.analytics_data
+    , coalesce(
+        recharge_subscriptions.order_day_of_week
+        , extract(dayofweek from date(recharge_subscriptions.created_at))
+    ) as order_day_of_week
+    , case
+        when recharge_subscriptions.has_queued_charges = 1 then TRUE 
+        when recharge_subscriptions.has_queued_charges = 0 then FALSE 
+    end as has_queued_charges
+    , coalesce(
+        recharge_subscriptions.order_day_of_month
+        , extract(day from date(recharge_subscriptions.created_at))
+    ) as order_day_of_month
+    , cast(recharge_subscriptions.shopify_product_id as int) as shopify_product_id
+    , cast(recharge_subscriptions.shopify_variant_id as int) as shopify_variant_id
+    , cast(recharge_subscriptions.cancellation_reason as string) as cancellation_reason
+    , cast(recharge_subscriptions.max_retries_reached as int) as max_retries_reached
+    , case
+        when recharge_subscriptions.order_interval_unit = 'year' then 'yearly'
+        when recharge_subscriptions.order_interval_unit = 'month' then 'monthly'
+        when recharge_subscriptions.order_interval_unit = 'week' then 'weekly'
+        when recharge_subscriptions.order_interval_unit = 'day' then 'daily'
+        when recharge_subscriptions.order_interval_unit is null then 'N/A'
+        else 'ERROR'
+    end as order_interval_unit
+    , cast(recharge_subscriptions.recharge_product_id as int) as recharge_product_id
+    , cast(recharge_subscriptions.next_charge_scheduled_at as datetime) as next_charge_scheduled_at
+    , cast(recharge_subscriptions.order_interval_frequency as int) as order_interval_frequency
+    , cast(recharge_subscriptions.charge_interval_frequency as int) as charge_interval_frequency
+    , cast(recharge_subscriptions.cancellation_reason_comments as string) as cancellation_reason_comments
+    , cast(recharge_subscriptions.expire_after_specific_number_of_charges as int) as expire_after_specific_number_of_charges
+from `wise-weaver-282922.raw_data_sandbox.acme1_recharge_subscriptions` as recharge_subscriptions
